d38f690dc8bfbc69eed9c65694e1c704667fcccb
zhouzhezz <zorichzorich@gmail.com>
2024-04-14 21:38:56 -0400

    add opt layout

--- utils/sugar.ml
+++ utils/sugar.ml
@@ -14,6 +14,7 @@ let map4 f (a, b, c, d) = (f a, f b, f c, f d)
 let map5 f (a, b, c, d, e) = (f a, f b, f c, f d, f e)
 let map6 f (a, b, c, d, e, g) = (f a, f b, f c, f d, f e, f g)
 let map7 f (a, b, c, d, e, g, h) = (f a, f b, f c, f d, f e, f g, f h)
+let opt_layout f = function None -> "none" | Some x -> f x
 
 let _deopt msg (x : 'a option) =
   match x with Some x -> x | None -> failwith msg
