aaf0d8207307aae6a94589d962ceb59e56a73b59
zhouzhezz <zorichzorich@gmail.com>
2024-05-11 12:24:15 -0400

    fix

--- normal5ty/frontend.ml
+++ normal5ty/frontend.ml
@@ -36,9 +36,7 @@ and core_type_desc_to_t t =
   | Ptyp_poly ([ lc ], ct) ->
       _failatwith __FILE__ __LINE__
         (spf "unimp: poly: lc: %s; ct: %s" lc.txt (layout_ ct))
-  | Ptyp_poly ([], _) ->
-      _failatwith __FILE__ __LINE__
-        (spf "unimp: poly: [], %s" @@ layout_ @@ desc_to_ct t)
+  | Ptyp_poly ([], ct) -> core_type_to_t ct
   | Ptyp_poly (_, _) ->
       _failatwith __FILE__ __LINE__
         (spf "unimp: poly: %s" @@ layout_ @@ desc_to_ct t)
